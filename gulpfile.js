b3bc8eb6af3df7a563b6ae47bfed28432a1425be
avarghese <arun.varghese@masergy.com>
2019-01-16 10:30:33 -0600

    fixed gulp taskflows, added async management

--- gulpfile.js
+++ gulpfile.js
@@ -18,9 +18,9 @@ let plumber = require('gulp-plumber');
 
 let paths = {
   scss: 'assets/css/resume.scss',
-  css: 'assets/css/resume.css',  
-  ts:'assets/js/resume.ts',
-  js:'assets/js/resume.js'
+  css: 'assets/css/resume.css',
+  ts: 'assets/js/resume.ts',
+  js: 'assets/js/resume.js'
 }
 
 const banner = ['/*\n',
@@ -32,8 +32,8 @@ const banner = ['/*\n',
 
 /** Miscellaneous Tasks **/
 
-function jshintReport(done) {
-  gulp.src([
+function jshintReport() {
+  return gulp.src([
       paths.ts,
       '*.js'
     ])
@@ -41,14 +41,12 @@ function jshintReport(done) {
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
-
-    done();
 };
 
-function format(done) {
-  gulp.src([
-       paths.scss,
-       paths.ts,
+function format() {
+  return gulp.src([
+      paths.scss,
+      paths.ts,
       'index.html',
       '*.{js,json}'
     ], {
@@ -57,8 +55,6 @@ function format(done) {
     .pipe(plumber())
     .pipe(beautify())
     .pipe(gulp.dest('./'));
-
-    done();
 };
 
 /** JavaScript Tasks **/
@@ -74,18 +70,16 @@ function cleanJs(done) {
   done();
 };
 
-function typescript(done) {
-  tsProject.src()
+function typescript() {
+  return tsProject.src()
     .pipe(plumber())
     .pipe(tsProject())
     .js.pipe(gulp.dest('assets/js'));
-
-    done();
 };
 
-function minifyJs(done) {
-  gulp.src([
-       paths.js
+function minifyJs() {
+  return gulp.src([
+      paths.js
     ])
     .pipe(plumber())
     .pipe(uglify())
@@ -96,12 +90,10 @@ function minifyJs(done) {
       suffix: '.min'
     }))
     .pipe(gulp.dest('assets/js'));
-
-    done();
 };
 
-function sourcemapJs(done) {
-  gulp.src([
+function sourcemapJs() {
+  return gulp.src([
       'assets/js/*.min.js',
       '!assets/js/*spec.js'
     ])
@@ -109,8 +101,6 @@ function sourcemapJs(done) {
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('assets/js'));
-
-    done();
 };
 
 /** CSS Tasks **/
@@ -126,21 +116,19 @@ function cleanCss(done) {
   done();
 };
 
-function scss(done) {
-  gulp.src('assets/css/**/*.scss')
+function scss() {
+  return gulp.src('assets/css/**/*.scss')
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
     }))
     .pipe(gulp.dest('assets/css'));
-
-    done();
 };
 
-function minifyCss(done) {
-  gulp.src([
-       paths.css
+function minifyCss() {
+  return gulp.src([
+      paths.css
     ])
     .pipe(plumber())
     .pipe(cleanCSS({
@@ -153,20 +141,16 @@ function minifyCss(done) {
       suffix: '.min'
     }))
     .pipe(gulp.dest('assets/css'));
-
-    done();
 };
 
-function sourcemapCss(done) {
-  gulp.src([
+function sourcemapCss() {
+  return gulp.src([
       'assets/css/*.min.css'
     ])
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('assets/css'));
-    
-    done();
 };
 
 /** Server Task **/
@@ -178,7 +162,7 @@ function serve(done) {
     },
     port: process.env.PORT || 4790
   });
-  
+
   done();
 };
 
