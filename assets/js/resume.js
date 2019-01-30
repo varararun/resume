41b45e7b425bb02a0a7a737441bb794048c632f6
avarghese <arun.varghese@masergy.com>
2019-01-29 23:56:20 -0600

    removed extra elements, js, refactored css, added li styling

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,63 +1,11 @@
-var _this = this;
 var Resume = {
-    initialize: function () {
-        Resume.loadEvents();
-        Resume.loadKeyboardEvents();
-    },
     closeLoader: function () {
         setTimeout(function () {
             $('#loader, .page-wrap').addClass('loaded');
             $('body').toggleClass('disable-scroll');
-        }, 1500);
+        }, 1000);
         setTimeout(function () {
             $('#loader').remove();
         }, 2000);
-    },
-    loadEvents: function () {
-        $('.nav-btn').click(function () {
-            $('.nav-btn').toggleClass('nav-close');
-            $('.nav').toggleClass('nav-open');
-            $('.page-wrap').toggleClass('page-wrap-disable');
-            $('body').toggleClass('disable-scroll');
-        });
-        $('.nav-menu>ul>li>a').click(function () {
-            $('.nav-btn').toggleClass('nav-close');
-            $('.nav').toggleClass('nav-open');
-            $('.page-wrap').toggleClass('page-wrap-disable');
-            $('body').toggleClass('disable-scroll');
-        });
-        $('.details-btn').click(function () {
-            $(_this).find('.details-icon').toggleClass('fa-caret-down').toggleClass('fa-caret-up');
-        });
-    },
-    loadKeyboardEvents: function () {
-        document.onkeydown = function (e) {
-            switch (e.keyCode) {
-                case 37: // left arrrow
-                    if ($("nav").hasClass("nav-open")) {
-                        $(".nav-btn").click();
-                    }
-                    break;
-                case 39: // right arrrow
-                    if (!$("nav").hasClass("nav-open")) {
-                        $(".nav-btn").click();
-                    }
-                    break;
-                case 27: // esc
-                    if ($("nav").hasClass("nav-open")) {
-                        $(".nav-btn").click();
-                    }
-                    break;
-                case 8: // delete/backspace
-                    if ($("nav").hasClass("nav-open")) {
-                        $(".nav-btn").click();
-                    }
-                    break;
-                case 32: // spacebar
-                    $(".nav-btn").click();
-                    break;
-            }
-        };
     }
 };
-Resume.initialize();
--- assets/js/resume.ts
+++ assets/js/resume.ts
@@ -1,63 +1,11 @@
 let Resume = {
-  initialize: () => {
-    Resume.loadEvents();
-    Resume.loadKeyboardEvents();
-  },
   closeLoader: () => {
     setTimeout(()=> {
       $('#loader, .page-wrap').addClass('loaded');   
       $('body').toggleClass('disable-scroll'); 
-    },1500);    
+    },1000);    
     setTimeout(()=> {
       $('#loader').remove();    
     },2000);     
-  },
-  loadEvents: () => {
-    $('.nav-btn').click(() => {
-      $('.nav-btn').toggleClass('nav-close');
-      $('.nav').toggleClass('nav-open');
-      $('.page-wrap').toggleClass('page-wrap-disable');
-      $('body').toggleClass('disable-scroll');
-    });
-    $('.nav-menu>ul>li>a').click(() => {
-      $('.nav-btn').toggleClass('nav-close');
-      $('.nav').toggleClass('nav-open');
-      $('.page-wrap').toggleClass('page-wrap-disable');
-      $('body').toggleClass('disable-scroll');
-    });
-    $('.details-btn').click(() => {
-      $(this).find('.details-icon').toggleClass('fa-caret-down').toggleClass('fa-caret-up');
-    });
-  },
-  loadKeyboardEvents: () => {
-    document.onkeydown = (e) => {
-      switch (e.keyCode) {
-        case 37: // left arrrow
-          if ($("nav").hasClass("nav-open")) {
-            $(".nav-btn").click();
-          }
-          break;
-        case 39: // right arrrow
-          if (!$("nav").hasClass("nav-open")) {
-            $(".nav-btn").click();
-          }
-          break;
-        case 27: // esc
-          if ($("nav").hasClass("nav-open")) {
-            $(".nav-btn").click();
-          }
-          break;
-        case 8: // delete/backspace
-          if ($("nav").hasClass("nav-open")) {
-            $(".nav-btn").click();
-          }
-          break;
-        case 32: // spacebar
-          $(".nav-btn").click();
-          break;
-      }
-    };
   }
 };
-
-Resume.initialize();
