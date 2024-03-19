a9bbe2e9ca0ff8a7795b535ffdeabd78da18e1ad
avarghese <arun.varghese@masergy.com>
2024-03-19 10:50:20 -0500

    html, manifest, icons

---
+++ sw.js
@@ -0,0 +1,48 @@
+const dataCacheName = 'avarghese-pwa';
+const cacheName = 'avarghese-pwa';
+const filesToCache = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'favicon.ico',
+    'assets/css/bundled.min.css',
+    'assets/js/bundled.min.js',
+    'assets/img/icons/icon.png'
+];
+
+//install the sw
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
